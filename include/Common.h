0cfb4f9482cab8ec2d1700345a3b276ecb6c99d8
authext <wolf.srb.ns@hotmail.com>
2016-01-06 21:55:44 +0100

    Add the missing commands and constants to Common.h

--- include/Common.h
+++ include/Common.h
@@ -6,9 +6,21 @@
 #define NAME_LENGTH 32
 #define BUFFER_SIZE 512
 
+#define SEPARATOR ':'
+#define TERMINATOR ';'
+#define ESCAPER '\\'
+
 enum Command
 {
-	CMD_LOGIN = 'i',
+	CMD_LOGIN  = 'i',
+	CMD_LOGOFF = 'o',
+	CMD_USERS  = 'u',
+	CMD_TALK   = 't',
+	CMD_SEND   = 's',
+	CMD_CLOSE  = 'c',
+	CMD_YES    = 'y',
+	CMD_NO     = 'n',
+	CMD_WTF    = 'w'
 };
 
 
